e8a70ec2b2852a23426b5d183267748ae21a5a82
Hcastro1515 <Elmenolx8@gmail.com>
2023-01-11 01:03:13 -0500

    added google addsense to every page including app.html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,23 @@
 <script>
-	import { fade } from "svelte/transition";
-	import "../app.css"
-	import Navbar from "../components/Navbar.svelte";
-	import Footer from "../components/Footer.svelte"
-	import { cubicIn } from "svelte/easing";
+	import { fade } from 'svelte/transition';
+	import '../app.css';
+	import Navbar from '../components/Navbar.svelte';
+	import Footer from '../components/Footer.svelte';
+	import { cubicIn } from 'svelte/easing';
 </script>
 
 <Navbar />
 
-<section class="container md:w-2/3 mx-auto py-12 px-6" in:fade={{easing: cubicIn, delay: 300}}>
-	<slot></slot>
+<svelte:head>
+	<script
+		async
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9306994878118987"
+		crossorigin="anonymous"
+	></script>
+</svelte:head>
+
+<section class="container md:w-2/3 mx-auto py-12 px-6" in:fade={{ easing: cubicIn, delay: 300 }}>
+	<slot />
 </section>
 
 <Footer />
